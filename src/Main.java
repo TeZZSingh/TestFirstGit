df8d9117f705478adba60618b143618961ea4eaa
Lalit Sharma <ks.lalit@gmail.com>
2023-05-04 17:16:19 +0530

    c

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world! by tej");
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world! by teja");
     }
 }
